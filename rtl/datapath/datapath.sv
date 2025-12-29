aed899b6c07deba13eb516e8beaaf3b1c879b9e6
Aike Gilabert <aikegilabert@gmail.com>
2025-12-29 13:12:17 +0100

    ROB integrated on the core

--- rtl/datapath/datapath.sv
+++ rtl/datapath/datapath.sv
@@ -11,22 +11,33 @@ module datapath
     input logic clk_i,
     input logic rstn_i
 );
-    logic valid_fetch;
+    logic commit_valid;
+    bus32_t commit_pc;
+    instruction_t commit_instr;
+    reg_addr_t commit_rd_addr;
+    bus32_t commit_result;
+    logic commit_write_enable;
+    logic commit_store_to_mem;
+    bus32_t commit_new_pc;
+    logic commit_branch_taken;
+
+    logic rob_full;
+
+    logic valid_fetch, valid_decode, valid_exe, valid_mem, valid_wb;
+    bus32_t pc_fetch, pc_decode, pc_exe, pc_mem, pc_wb;
 
     instruction_t instruction_d, instruction_q;
-    bus32_t pc_fetch, pc_decode;
-    logic valid_decode;
 
     logic stall_fetch;
 
-    assign stall_fetch = stall | ~valid_decode;
+    assign stall_fetch = stall;
 
     // Fetch
     fetch fetch_inst (
         .clk_i(clk_i),
         .rstn_i(rstn_i),
-        .taken_branch_i(mem_to_wb_q.branch_taken),
-        .new_pc_i(mem_to_wb_q.branched_pc),
+        .taken_branch_i(commit_branch_taken),
+        .new_pc_i(commit_new_pc),
         .stall_i(stall_fetch),
         .pc_o(pc_fetch),
         .instr_o(instruction_d),
@@ -44,25 +55,25 @@ module datapath
     end
 */
     always_ff @(negedge rstn_i, posedge clk_i) begin
-        if (~rstn_i) begin
+        if (~rstn_i || commit_branch_taken) begin
             instruction_q <= '0;
             pc_decode <= '0;
             valid_decode <= 1'b0;
-        end else if (exe_to_mem_d.branch_taken == 1'b1 ||
-                     mem_to_wb_d.branch_taken == 1'b1 ||
-                     mem_to_wb_q.branch_taken == 1'b1) begin
-            instruction_q <= NOP_INSTR_HEX;
-            pc_decode <= '0;
-            valid_decode <= 1'b0;
+            //end else if (exe_to_mem_d.branch_taken == 1'b1 ||
+            //         mem_to_wb_d.branch_taken == 1'b1 ||
+            //         mem_to_wb_q.branch_taken == 1'b1) begin
+            //instruction_q <= NOP_INSTR_HEX;
+            //pc_decode <= '0;
+            //valid_decode <= 1'b0;
         end else begin
             if (!stall) begin
                 instruction_q <= instruction_d; // captura nueva instrucción
                 pc_decode <= pc_fetch;
-                valid_decode <= 1'b1;
+                valid_decode <= valid_fetch;
             end else begin
                 instruction_q <= instruction_q; // hold
                 pc_decode <= pc_decode;         // hold
-                valid_decode <= 1'b1;
+                valid_decode <= valid_decode;
             end
         end
     end
@@ -70,9 +81,12 @@ module datapath
     // Decode
     decode_to_exe_t decode_to_exe_d, decode_to_exe_q;
 
+    rob_idx_t rob_entry_decode;
+
     decoder decoder_inst (
         .pc_i(pc_decode),
         .instr_i(instruction_q),
+        .rob_idx_i(rob_entry_decode),
         .instr_decoded_o(decode_to_exe_d.instr)
     );
 
@@ -86,9 +100,9 @@ module datapath
         .rstn_i(rstn_i),
         .addr_rs1_i(decode_to_exe_d.instr.addr_rs1),
         .addr_rs2_i(decode_to_exe_d.instr.addr_rs2),
-        .addr_rd_i(mem_to_wb_q.instr.addr_rd),
-        .data_rd_i(mem_to_wb_q.result),
-        .write_enable_i(mem_to_wb_q.instr.write_enable),
+        .addr_rd_i(commit_rd_addr),
+        .data_rd_i(commit_result),
+        .write_enable_i(commit_write_enable),
         .data_rs1_o(decode_to_exe_d.data_rs1),
         .data_rs2_o(decode_to_exe_d.data_rs2)
     );
@@ -100,6 +114,8 @@ module datapath
     assign rs2_d = decode_to_exe_d.instr.addr_rs2;
 
     logic hazard_rs1, hazard_rs2, hazard_from_pipe;
+
+    logic hazard_rob;
     logic stall_from_exe;
 
     always_comb begin
@@ -118,7 +134,9 @@ module datapath
         hazard_rs1 |= reg_hazard(rs1_d, mem_to_wb_q.instr.addr_rd, mem_to_wb_q.instr.write_enable);
         hazard_rs2 |= reg_hazard(rs2_d, mem_to_wb_q.instr.addr_rd, mem_to_wb_q.instr.write_enable);
 
-        stall = hazard_rs1 | hazard_rs2 | stall_from_exe | hazard_from_pipe | (~valid_fetch);
+        //stall = hazard_rs1 | hazard_rs2 | stall_from_exe | hazard_from_pipe | (~valid_fetch) | rob_full;
+
+        stall = hazard_rob | stall_from_exe | (~valid_fetch) | rob_full;
     end
 
     assign decode_to_exe_d.valid = ~stall & valid_decode;
@@ -128,11 +146,11 @@ module datapath
             decode_to_exe_q <= '0;
         end else begin
             if (!stall) begin
-                if (exe_to_mem_d.branch_taken == 1'b1) begin
-                    decode_to_exe_q <= NOP_INSTR;
-                end else begin
+                //if (exe_to_mem_d.branch_taken == 1'b1) begin
+                //    decode_to_exe_q <= NOP_INSTR;
+                //end else begin
                     decode_to_exe_q <= decode_to_exe_d;
-                end
+                    //end
             end else begin
                 if (!stall_from_exe) begin
                     decode_to_exe_q <= NOP_INSTR;
@@ -182,17 +200,63 @@ module datapath
         end
     end
 
+    rob rob_inst (
+        .clk_i(clk_i),
+        .rstn_i(rstn_i),
+        //.flush_i(mem_to_wb_q.branch_taken),
+        .valid_decode_i(decode_to_exe_d.valid),
+        .pc_i(decode_to_exe_d.instr.pc),
+        .instr_i(decode_to_exe_d.instr.instr),
+        .rd_addr_i(decode_to_exe_d.instr.addr_rd),
+        .write_enable_i(decode_to_exe_d.instr.write_enable),
+        .store_to_mem_i(decode_to_exe_d.instr.store_to_mem),
+
+        .rob_entry_alloc_o(rob_entry_decode),
+
+        .rob_entry_commit_i(mem_to_wb_q.instr.rob_idx),
+        .valid_wb_i(mem_to_wb_q.valid),
+        .result_i(mem_to_wb_q.result),
+        .new_pc_i(mem_to_wb_q.branched_pc),
+        .branch_taken_i(mem_to_wb_q.branch_taken),
+
+        .commit_valid_o(commit_valid),
+        .commit_pc_o(commit_pc),
+        .commit_instr_o(commit_instr),
+        .commit_rd_addr_o(commit_rd_addr),
+        .commit_result_o(commit_result),
+        .commit_write_enable_o(commit_write_enable),
+        .commit_store_to_mem_o(commit_store_to_mem),
+        .commit_new_pc_o(commit_new_pc),
+        .commit_branch_taken_o(commit_branch_taken),
+
+        .rob_full_o(rob_full),
+
+        .rs1_addr_i(decode_to_exe_d.instr.addr_rs1),
+        .rs2_addr_i(decode_to_exe_d.instr.addr_rs2),
+        .hazard_o(hazard_rob)
+    );
 
     // Writeback
 
     always_ff @(posedge clk_i) begin
-        if (mem_to_wb_q.valid) begin
+        if (commit_valid) begin
             print_commit(
-                mem_to_wb_q.instr.pc,
-                mem_to_wb_q.instr.instr.instruction,
-                mem_to_wb_q.result
+                commit_pc,
+                commit_instr,
+                commit_result
             );
         end
+    end
+
+    always_comb begin
+        valid_exe = decode_to_exe_q.valid;
+        pc_exe    = decode_to_exe_q.instr.pc;
+
+        valid_mem = exe_to_mem_q.valid;
+        pc_mem    = exe_to_mem_q.instr.pc;
+
+        valid_wb  = mem_to_wb_q.valid;
+        pc_wb     = mem_to_wb_q.instr.pc;
     end
 
 endmodule
